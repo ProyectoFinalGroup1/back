7d2d075f2dc75e897d66b600172c95f1cfed731e
JDamianDelgado <joakodelgado2013@gmail.com>
2025-02-19 16:30:32 -0300

    conectando con Supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupabaseModule } from './Config/supabase.module';
+
+console.log('ESE : ', process.env.DATABASE_URL);
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/Config/.env',
+    }),
+    SupabaseModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.js,.ts}'],
+      synchronize: true,
+      migrationsRun: true,
+      logging: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
