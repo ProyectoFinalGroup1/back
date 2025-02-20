3a9db27f31b257aaa55c81950e45bb6270b40d01
Agata <agata.morales92@gmail.com>
2025-02-20 16:38:28 -0300

    feat: implementación de Cloudinary y Swagger

--- src/Modules/Inhumado/inhumado.controller.ts
+++ src/Modules/Inhumado/inhumado.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { inhumadosService } from './inhumado.service';
 
+@ApiTags('Inhumados')
 @Controller('inhumados')
 export class InhumadoController {
   constructor(private readonly inhumadosService: inhumadosService) {}
 
   @Get('seeder')
+  @ApiOperation({ summary: 'Obtener seeder :D' })
   async seed() {
     await this.inhumadosService.seed();
     return { message: 'sedder exitoso' }; //borrar
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener lista de todos los inhumados' })
   async allInhumados() {
     const datos = await this.inhumadosService.allInhumados();
     return datos;
