e9ec4515339b5d32bf7c30fbecf3830970cb23d6
JDamianDelgado <joakodelgado2013@gmail.com>
2025-02-27 12:20:16 -0300

    se implemento funciones en mensajesvirgen y publicaciones

--- src/Modules/mensajesVirgen/menVir.controller.ts
+++ src/Modules/mensajesVirgen/menVir.controller.ts
@@ -1,38 +1,60 @@
-import { MensajesVirgenService } from "./menVir.service";
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, UseGuards } from "@nestjs/common";
-import { ApiOperation } from "@nestjs/swagger";
-import { AuthGuard } from "../Guards/Jwt/AuthGuards";
-import { Role } from "../Guards/Roles/roles.enum";
-import { Roles } from "../Guards/Roles/roles.decorator";
-import { RolesGuard } from "../Guards/Roles/Roles.guard";
-import { MensajeAVirgen } from "src/Entities/mensajesVirgen.entity";
+import { MensajesVirgenService } from './menVir.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from '../Guards/Jwt/AuthGuards';
+import { Role } from '../Guards/Roles/roles.enum';
+import { Roles } from '../Guards/Roles/roles.decorator';
+import { RolesGuard } from '../Guards/Roles/Roles.guard';
+import { MensajeAVirgen } from 'src/Entities/mensajesVirgen.entity';
 
-
-@Controller("mensajesVirgen")
+@Controller('mensajesVirgen')
 export class MensajesVirgenController {
-    constructor (private readonly mensajesVirgenService: MensajesVirgenService){}
+  constructor(private readonly mensajesVirgenService: MensajesVirgenService) {}
 
-@UseGuards(AuthGuard)
-@Get()
-@ApiOperation({ summary: 'Obtener los mensajes a la virgen por id del usuario' })
-async getMensajesVirgen() {
+  @UseGuards(AuthGuard)
+  @Get()
+  @ApiOperation({
+    summary: 'Obtener los mensajes a la virgen por id del usuario',
+  })
+  async getMensajesVirgen() {
     return await this.mensajesVirgenService.getMensajesVirgen();
-}
-
-
-@UseGuards(AuthGuard)
-@Post("addMensajeVirgen")
-@ApiOperation({ summary: 'Agregar un mensaje a la virgen' })
-async addMensajeVirgen(@Body() mensajeVirgen: MensajeAVirgen){
-    return await this.mensajesVirgenService.addMensajeVirgen(mensajeVirgen)
   }
 
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard)
+  @Get('filter')
+  async filterMSsjs() {
+    return await this.mensajesVirgenService.filterMSsjs();
+  }
 
-@Roles(Role.Admin)
-@UseGuards(AuthGuard, RolesGuard)
-@Delete(':id')
-@ApiOperation({ summary: 'Eliminar una mensajes a la virgen por ID' })
-async deleteMensajeVirgen(@Param('id') id: string) {
-return await this.mensajesVirgenService.deleteMensajeVirgen(id);
+  @UseGuards(AuthGuard)
+  @Post('addMensajeVirgen')
+  @ApiOperation({ summary: 'Agregar un mensaje a la virgen' })
+  async addMensajeVirgen(@Body() mensajeVirgen: Partial<MensajeAVirgen>) {
+    return await this.mensajesVirgenService.addMensajeVirgen(mensajeVirgen);
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard)
+  @Patch(':id')
+  async aprobarMsj(@Param('id') id: string) {
+    return this.mensajesVirgenService.aprobado(id);
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar una mensajes a la virgen por ID' })
+  async deleteMensajeVirgen(@Param('id') id: string) {
+    return await this.mensajesVirgenService.deleteMensajeVirgen(id);
   }
 }
--- src/Modules/publicaciones/publi.controller.ts
+++ src/Modules/publicaciones/publi.controller.ts
@@ -1,48 +1,67 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Request, UseGuards } from "@nestjs/common";
-import { PublicacionesService } from "./publi.service";
-import { ApiOperation } from "@nestjs/swagger";
-import { AuthGuard } from "../Guards/Jwt/AuthGuards";
-import { Publicacion } from "src/Entities/publicaciones.entity";
-import { Role } from "../Guards/Roles/roles.enum";
-import { Roles } from "../Guards/Roles/roles.decorator";
-import { RolesGuard } from "../Guards/Roles/Roles.guard";
-import { CreatePublicacionDto } from "../DTO/publicacionDto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { PublicacionesService } from './publi.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from '../Guards/Jwt/AuthGuards';
+import { Role } from '../Guards/Roles/roles.enum';
+import { Roles } from '../Guards/Roles/roles.decorator';
+import { RolesGuard } from '../Guards/Roles/Roles.guard';
+import { CreatePublicacionDto } from '../DTO/publicacionDto';
 
-
-
-@Controller("publicaciones")
+@Controller('publicaciones')
 export class PublicacionesController {
-    constructor (private readonly publicacionesService: PublicacionesService){}
+  constructor(private readonly publicacionesService: PublicacionesService) {}
 
-
-@UseGuards(AuthGuard)
-@Get(':nombreInhumado')
-@ApiOperation({ summary: 'Obtener publicaciones por nombre de inhumado' })
-async getPublicacionesByInhumado(@Param('nombreInhumado') nombre: string) {
-    return await this.publicacionesService.getPublicacionesByInhumado(nombre);
-}
-
-
-@UseGuards(AuthGuard)
-@Post("addPublicacion")
-@ApiOperation({ summary: 'Agregar un publicacion' })
-async addPublicacion(@Body() publicacionDto: CreatePublicacionDto): Promise<string>{
-    return await this.publicacionesService.addPublicacion(publicacionDto)
+  //admin
+  @Get('pendientes')
+  async publicacionesPendientes() {
+    return await this.publicacionesService.pendientes();
   }
 
-@Roles(Role.Admin)
-@UseGuards(AuthGuard, RolesGuard) // Solo admin
-@Put('aprobar/:id')
-@ApiOperation({ summary: 'Aprobar una publicación' })
-async aprobarPublicacion(@Param('id') id: string) {
-    return await this.publicacionesService.aprobarPublicacion(id);
-}
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard, RolesGuard) // Solo admin
+  @Get('')
+  async allPublicaciones() {
+    return this.publicacionesService.allPublication();
+  }
 
-@Roles(Role.Admin)
-@UseGuards(AuthGuard, RolesGuard)
-@Delete(':id')
-@ApiOperation({ summary: 'Eliminar una publicación por ID' })
-async deletePublicacion(@Param('id') id: string) {
-return await this.publicacionesService.deletePublicacion(id);
+  @UseGuards(AuthGuard)
+  @Post('addPublicacion')
+  @ApiOperation({ summary: 'Agregar un publicacion' })
+  async addPublicacion(
+    @Body() publicacionDto: CreatePublicacionDto,
+  ): Promise<string> {
+    return await this.publicacionesService.addPublicacion(publicacionDto);
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard, RolesGuard) // Solo admin
+  @Patch(':id')
+  @ApiOperation({ summary: 'Aprobar una publicación' })
+  async aprobarPublicacion(@Param('id') id: string) {
+    return await this.publicacionesService.aprobarPublicacion(id);
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar una publicación por ID' })
+  async deletePublicacion(@Param('id') id: string) {
+    return await this.publicacionesService.deletePublicacion(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':nombreInhumado')
+  @ApiOperation({ summary: 'Obtener publicaciones por nombre de inhumado' })
+  async getPublicacionesByInhumado(@Param('nombreInhumado') nombre: string) {
+    return await this.publicacionesService.getPublicacionesByInhumado(nombre);
   }
 }
