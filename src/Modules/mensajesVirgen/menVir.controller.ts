099237eb6488b8b3f8713e1afc6b5a6bdac94366
JDamianDelgado <joakodelgado2013@gmail.com>
2025-02-28 13:41:41 -0300

    se creo el subir imagenes con cloudinary mediante formularios en inhumadosadd, publicaciones , mensajes Virgen

--- src/Modules/mensajesVirgen/menVir.controller.ts
+++ src/Modules/mensajesVirgen/menVir.controller.ts
@@ -1,15 +1,21 @@
 import { MensajesVirgenService } from './menVir.service';
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
+  FileTypeValidator,
   Get,
+  MaxFileSizeValidator,
   Param,
+  ParseFilePipe,
   ParseUUIDPipe,
   Patch,
   Post,
   Put,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from '../Guards/Jwt/AuthGuards';
@@ -17,6 +23,7 @@ import { Role } from '../Guards/Roles/roles.enum';
 import { Roles } from '../Guards/Roles/roles.decorator';
 import { RolesGuard } from '../Guards/Roles/Roles.guard';
 import { MensajeAVirgen } from 'src/Entities/mensajesVirgen.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('mensajesVirgen')
 export class MensajesVirgenController {
@@ -40,9 +47,39 @@ export class MensajesVirgenController {
 
   @UseGuards(AuthGuard)
   @Post('addMensajeVirgen')
+  @UseInterceptors(FileInterceptor('file'))
   @ApiOperation({ summary: 'Agregar un mensaje a la virgen' })
-  async addMensajeVirgen(@Body() mensajeVirgen: Partial<MensajeAVirgen>) {
-    return await this.mensajesVirgenService.addMensajeVirgen(mensajeVirgen);
+  async addMensajeVirgen(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 200000000,
+            message: 'El tamaño de la imagen debe ser inferior a 200MB',
+          }),
+          new FileTypeValidator({
+            fileType: /^(image\/jpeg|image\/png)$/,
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() mensajeVirgen: Partial<MensajeAVirgen>,
+  ) {
+    try {
+      const imgCloudinary = await this.mensajesVirgenService.uploadImage(file);
+
+      const newMsjVirgen = await this.mensajesVirgenService.addMensajeVirgen(
+        mensajeVirgen,
+        imgCloudinary,
+      );
+      return {
+        message: 'Publicacion creada con exito a la espera de su aprobacion',
+        newMsjVirgen,
+      };
+    } catch (error) {
+      throw new BadRequestException(error.mensagge);
+    }
   }
 
   @Roles(Role.Admin)
@@ -63,7 +100,13 @@ export class MensajesVirgenController {
   @UseGuards(AuthGuard)
   @Put('editar/:id')
   @ApiOperation({ summary: 'Editar un mensaje a la virgen' })
-  async updateMensajeVirgen(@Param('id', ParseUUIDPipe) id: string , @Body() mensajeVirgen: Partial <MensajeAVirgen>){
-    return await this.mensajesVirgenService.updateMensajeVirgen(id, mensajeVirgen);
+  async updateMensajeVirgen(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() mensajeVirgen: Partial<MensajeAVirgen>,
+  ) {
+    return await this.mensajesVirgenService.updateMensajeVirgen(
+      id,
+      mensajeVirgen,
+    );
   }
 }
